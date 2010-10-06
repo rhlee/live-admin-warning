0ea29e644bfe4cffbdffff9dec46fccd537bf663
Richard H Lee <richard.lee@canonical.com>
2010-10-06 14:08:31 +0100

    Plugin

---
+++ live-admin-warning.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Live admin warning
+Plugin URI: http://github.com/rhlee/live-admin-warning
+Description: A plugin to warn you that you  are on a live Wordpress admin
+Version: 0.1
+Author: Richard H Lee
+Author URI: http://github.com/rhlee
+License: GPL
+*/
+?>
