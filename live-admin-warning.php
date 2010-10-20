e4551f581edfaab57c22cbd50f5d2d224de0a619
Richard H Lee <richard.lee@canonical.com>
2010-10-20 10:40:54 +0100

    Set default to not show

--- live-admin-warning.php
+++ live-admin-warning.php
@@ -41,7 +41,7 @@ class live_admin_warning_class
 	function get_options()
 	{
 		$default_options = array(
-			'live_admin_warning_show' => '1',
+			'live_admin_warning_show' => '0',
 			'live_admin_warning_message' => 'Warning! This is a live site!'
 		);
 		
